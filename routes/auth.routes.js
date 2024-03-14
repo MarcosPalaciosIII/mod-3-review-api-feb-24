0659f9f7627afb8b02974ac23b925d612c94b999
Marcos Palacios <mp2772@gmail.com>
2024-03-14 19:50:22 -0400

    updated jwt content

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,26 +17,26 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
-router.post("/signup", (req, res, next) => {
+router.post("/signup", (req, res) => {
 	const { email, password, name } = req.body;
 
 	// Check if email or password or name are provided as empty strings
 	if (email === "" || password === "" || name === "") {
-		res.status(400).json({ message: "Provide email, password and name" });
+		res.json({ message: "Provide email, password and name" });
 		return;
 	}
 
 	// This regular expression check that the email is of a valid format
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 	if (!emailRegex.test(email)) {
-		res.status(400).json({ message: "Provide a valid email address." });
+		res.json({ message: "Provide a valid email address." });
 		return;
 	}
 
 	// This regular expression checks password for special characters and minimum length
 	const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 	if (!passwordRegex.test(password)) {
-		res.status(400).json({
+		res.json({
 			message:
 				"Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
 		});
@@ -48,7 +48,7 @@ router.post("/signup", (req, res, next) => {
 		.then((foundUser) => {
 			// If the user with the same email already exists, send an error response
 			if (foundUser) {
-				res.status(400).json({ message: "User already exists." });
+				res.json({ message: "User already exists." });
 				return;
 			}
 
@@ -69,18 +69,18 @@ router.post("/signup", (req, res, next) => {
 			const user = { email, name, _id };
 
 			// Send a json response containing the user object
-			res.status(201).json({ user: user });
+			res.json({ user: user });
 		})
-		.catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+		.catch((err) => res.json(err)); // In this case, we send error handling to the error handling middleware.
 });
 
 // POST  /auth/login - Verifies email and password and returns a JWT
-router.post("/login", (req, res, next) => {
+router.post("/login", (req, res) => {
 	const { email, password } = req.body;
 
 	// Check if email or password are provided as empty string
 	if (email === "" || password === "") {
-		res.status(400).json({ message: "Provide email and password." });
+		res.json({ message: "Provide email and password." });
 		return;
 	}
 
@@ -89,7 +89,7 @@ router.post("/login", (req, res, next) => {
 		.then((foundUser) => {
 			if (!foundUser) {
 				// If the user is not found, send an error response
-				res.status(401).json({ message: "User not found." });
+				res.json({ message: "User not found." });
 				return;
 			}
 
@@ -113,24 +113,24 @@ router.post("/login", (req, res, next) => {
 				});
 
 				// Send the token as the response
-				res.status(200).json({ authToken: authToken });
+				res.json({ authToken: authToken });
 			} else {
-				res.status(401).json({
+				res.json({
 					message: "Unable to authenticate the user",
 				});
 			}
 		})
-		.catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+		.catch((err) => res.json(err)); // In this case, we send error handling to the error handling middleware.
 });
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
-router.post("/verify", isAuthenticated, (req, res, next) => {
+router.post("/verify", isAuthenticated, (req, res) => {
 	// If JWT token is valid the payload gets decoded by the
 	// isAuthenticated middleware and is made available on `req.payload`
 	console.log(`req.payload`, req.payload);
 
 	// Send back the token payload object containing the user data
-	res.status(200).json({ success: true, data: req.payload });
+	res.json({ success: true, data: req.payload });
 });
 
 module.exports = router;
